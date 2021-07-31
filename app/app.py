80a5a0f6e75eb98f9b19309ae204d79dc1e06c08
tanvi3110 <tanvishah3110@gmail.com>
2021-07-31 15:45:48 -0400

    Task: part 1 ran successfully

--- app/app.py
+++ app/app.py
@@ -1,4 +1,6 @@
 from flask import Flask, Markup, render_template, make_response, request, jsonify
+from logic import add
+
 # Create Flask's `app` object
 app = Flask(
     __name__,
@@ -15,6 +17,36 @@ def hello():
     my_dict = {'key': 'dictionary value'}
     headers = {"Content-Type": "application/json"}
     return make_response(jsonify(my_dict), 200, headers)
+
+
+@app.route("/logic")
+def logic():
+    value = add(5)
+    return str(value)
+
+
+@app.route("/markup")
+def markup():
+    return Markup("<h1>Hello World!</h1>")
+
+
+@app.route("/templates")
+def hello_template():
+    return render_template("index.html")
+
+
+@app.route("/response")
+def response():
+    headers = {"Content-Type": "application/json"}
+    return make_response('it worked!', 200, headers)
+
+
+@app.route("/get", methods=['GET'])
+def get_hello():
+    if request.method != 'GET':
+        return make_response('Malformed request', 400)
+    headers = {"Content-Type": "application/json"}
+    return make_response('it worked!', 100, headers)
 
 
 if __name__ == '__main__':
